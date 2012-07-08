a889fedf94c233b8c1fc689612bffd3b5acda59b
longfin <longfinfunnel@gmail.com>
2012-07-09 02:26:59 +0900

    remove unnecessary code

--- src/warklet/core.clj
+++ src/warklet/core.clj
@@ -1,11 +1,1 @@
-(ns warklet.core
-  (:require [cljs.closure :as closure]))
-
-(defn build-bookmark-script [user-id]
-  (assert user-id)
-  (closure/build
-   ['(require '[clojure.browser.dom :as dom])
-    `(.log js/console ~user-id)
-    `(dom/get-element :li)]
-   {:optimizations :advanced}))
-  
+(ns warklet.core)
