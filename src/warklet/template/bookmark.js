c0f3e9d885bb0b90c6b8495ece87fdaa7ccb1f06
longfin <longfinfunnel@gmail.com>
2012-07-10 00:22:05 +0900

    change bookmark.js creation logic

--- src/warklet/template/bookmark.js
+++ src/warklet/template/bookmark.js
@@ -1,6 +1,6 @@
 javascript:(function () {
   var newScript = document.createElement('script');
   newScript.type = 'text/javascript';
-  newScript.src = 'http://warklet-dev.elasticbeanstalk.com/user/{{user-id}}/script';
+  newScript.src = '{{script-url}}';
   document.getElementsByTagName('body')[0].appendChild(newScript);
 })();
--- src/warklet/views/js.clj
+++ src/warklet/views/js.clj
@@ -1,15 +1,20 @@
 (ns warklet.views.js
-  (:use [noir.core :only [defpage]]
+  (:use [noir.core :only [defpage url-for]]
         [noir.request :only [ring-request]]
         [clojure.java.io :only [resource]]))
 
-(defpage "/users/:id/bookmark" {user-id :id}
+(defpage bookmark "/users/:id/bookmark" {user-id :id}
   {:headers {"content-type" "text/javascript; charset=UTF-8"}
-   :body (clojure.string/replace
-          (slurp (resource "warklet/template/bookmark.js"))
-          "{{user-id}}"
-          user-id)})
+   :body (let [source (slurp (resource "warklet/template/bookmark.js"))
+               request (ring-request)
+               script-url (str (name (:scheme request))
+                               "://"
+                               (:server-name request)
+                               ":"
+                               (:server-port request)
+                               (url-for script {:id user-id}))]
+           (clojure.string/replace source "{{script-url}}" script-url))})
 
-(defpage "/users/:id/script" {user-id :id}
+(defpage script "/users/:id/script" {user-id :id}
   {:headers {"content-type" "text/javascript; charset=UTF-8"}
    :body user-id})
