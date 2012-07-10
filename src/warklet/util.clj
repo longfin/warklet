10f6cf0405d4530499cc67a46ce9df7a7ca8c39d
longfin <longfinfunnel@gmail.com>
2012-07-11 00:31:02 +0900

    add warklet.util and warklet.util/hash-password

---
+++ src/warklet/util.clj
@@ -0,0 +1,10 @@
+(ns warklet.util
+  (:use [clojure.string :only [join]]))
+
+(defn hash-password [password]
+  (let [md (java.security.MessageDigest/getInstance "SHA-512")
+        encoder (sun.misc.BASE64Encoder.)
+        salt "warkletsaltinheaven"]
+    (.update md (.getBytes salt "UTF-8"))
+    (join "" (map #(Integer/toHexString (bit-and % 0xff))
+                  (.digest md (.getBytes password "UTF-8"))))))
