efa6048963e42b983f7156f3245a01439f26ddf9
longfin <longfinfunnel@gmail.com>
2012-08-08 02:41:46 +0900

    add https support

--- src/js/bookmark.js
+++ src/js/bookmark.js
@@ -2,6 +2,13 @@ javascript:(function () {
     var newScript = document.createElement('script');
     newScript.type = 'text/javascript';
     newScript.src = '{{script-url}}';
+    if (location.protocol != "http") {
+        newScript.src = newScript.src.replace(/(.*):\/\//, 
+                                              location.protocol+"//");
+        console.log("!"+newScript.src);
+        newScript.src = newScript.src.replace(/:{1}\d{1,}/, "");
+        console.log("@"+newScript.src);
+    }
     if (newScript.addEventListener) {
         newScript.addEventListener("error", function(e){
             alert("Fail to load init script...");
